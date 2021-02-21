d61bc7c69a3fda088d76166a18cedd0720d27548
bennyhuo <bennyhuo@kotliner.cn>
2021-02-21 10:02:03 +0800

    cleanup invalid configurations.

--- luajava/src/main/jni/Application.mk
+++ luajava/src/main/jni/Application.mk
@@ -1,12 +1,9 @@
 APP_ABI := all
 
-APP_LDFLAGS += \
-    -Wl,--exclude-libs=ALL
-
+APP_LDFLAGS += -Wl,--exclude-libs=ALL
 
 APP_OPTIM   := release
 
 APP_MODULES := liblua luajava
 
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_PLATFORM 	:= android-14
+APP_PLATFORM 	:= android-16
